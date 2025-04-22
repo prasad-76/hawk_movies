47d8a8d0e6f990349d724912be01c5e51bf184f3
prasad-76 <prasadbagad76@gmail.com>
2025-04-22 19:20:35 +0530

    Delete server/routes directory

--- server/routes/creations.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { discoverCreations, } from "../controllers/creation.controller.js";
-
-const router = express.Router();
-
-router.get("/discover/:endpoint", discoverCreations);
-
-export default router;
--- server/routes/user.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { find } from "../controllers/user.controller.js";
-
-const router = express.Router();
-
-router.get("/profile", find);
-
-export default router;
